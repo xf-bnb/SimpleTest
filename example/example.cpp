a4e50a640687c35a2739de8ef83d511c86e7c198
FrankXiong <Frank_Xiong_@outlook.com>
2020-01-03 15:38:26 +0800

    implement xf test

---
+++ example/example.cpp
@@ -0,0 +1,39 @@
+#include "../include/xf_simple_test.h"
+
+namespace t
+{
+    _xfTest(test_1)
+    {
+        _xfExpect(1 == 2);  // error
+        _xfExpect(1 < 2);   // ok
+        _xfExpect(1 > 2);   // error
+    }
+
+    _xfTest(test_2)
+    {
+        _xfAssert(1 == 2);  // error
+        _xfAssert(1 < 2);   // ignore
+        _xfAssert(1 > 2);   // ignore
+    }
+
+}
+
+_xfTest(test_3)
+{
+    int a = 4, b = 2;
+
+    _xfExpect(a > b);       // ok
+    _xfExpect(a < b);       // error
+    _xfExpect(2 == a + b);  // error
+    _xfExpect(2 == a - b);  // ok
+    _xfExpect(2 == a / b);  // ok
+    _xfAssert(2 == a * b);  // error
+    _xfExpect(2 == a % b);  // ignore
+}
+
+int main()
+{
+    xf::test::Test();
+
+    return 0;
+}
